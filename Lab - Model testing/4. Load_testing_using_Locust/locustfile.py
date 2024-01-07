c2e35ac9162029f347b695c49c533c54c718cc33
hassanoubrahim <65039956+hassanoubrahim@users.noreply.github.com>
2024-01-07 12:02:57 +0100

    Add files via upload

---
+++ Lab - Model testing/4. Load_testing_using_Locust/locustfile.py
@@ -0,0 +1,12 @@
+from locust import HttpUser, task
+from random import choice
+from string import ascii_uppercase
+
+class User(HttpUser):
+    @task
+    def predict(self):
+        payload = {
+            "question": ''.join(choice(ascii_uppercase) for i in range(20)),
+            "context": ''.join(choice(ascii_uppercase) for i in range(20))
+        }
+        self.client.post("/question_answering", json=payload)
